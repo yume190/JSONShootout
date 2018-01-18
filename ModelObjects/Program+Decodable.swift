79750d8b44f64972595f0544f8f70ed8c8d993aa
yume190 <yume190@gmail.com>
2018-01-18 17:49:10 +0800

    remove Decodable test

--- ModelObjects/Program+Decodable.swift
+++ ModelObjects/Program+Decodable.swift
@@ -1,26 +1,27 @@
+////
+////  Program+Decodable.swift
+////  JSONShootout
+////
+////  Created by Alessandro Orrù on 25/10/16.
+////  Copyright © 2016 SwiftBit. All rights reserved.
+////
 //
-//  Program+Decodable.swift
-//  JSONShootout
+//import Foundation
+//import Decodable
 //
-//  Created by Alessandro Orrù on 25/10/16.
-//  Copyright © 2016 SwiftBit. All rights reserved.
-//
+//extension Program: Decodable {
+//    public static func decode(_ json: Any) throws -> Program {
+//        return try Program(
+//            title: json => "Title",
+//            chanId: json => "Channel" => "ChanId",
+//            //          startTime = json => "StartTime",
+//            //          endTime = json => "EndTime",
+//            description: json => "Description",
+//            subtitle: json => "SubTitle",
+//            recording: json => "Recording",
+//            season: Int(json => "Season" as String),
+//            episode: Int(json => "Episode" as String)
+//        )
+//    }
+//}
 
-import Foundation
-import Decodable
-
-extension Program: Decodable {
-    public static func decode(_ json: Any) throws -> Program {
-        return try Program(
-            title: json => "Title",
-            chanId: json => "Channel" => "ChanId",
-            //          startTime = json => "StartTime",
-            //          endTime = json => "EndTime",
-            description: json => "Description",
-            subtitle: json => "SubTitle",
-            recording: json => "Recording",
-            season: Int(json => "Season" as String),
-            episode: Int(json => "Episode" as String)
-        )
-    }
-}
